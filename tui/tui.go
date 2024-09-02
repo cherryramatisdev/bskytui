9a5b9c4329fdf15fb75a15fb0c5499e5d769e020
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-09-02 00:18:38 -0300

    feat: Add support for a detail page

--- tui/tui.go
+++ tui/tui.go
@@ -1,47 +1,76 @@
 package tui
 
 import (
+	"fmt"
+	"slices"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
 	"github.com/charmbracelet/x/term"
 	"github.com/cherryramatisdev/bskytui/sdk"
 )
+
+type Author struct {
+	DisplayName string
+	Handle      string
+}
 
 type Post struct {
 	ID      string
 	Content string
 	Summary string
-	Author  string
+	Author  Author
 	Langs   []string
 }
 
 func (p Post) Title() string {
-	return p.Author
+	return fmt.Sprintf("%s-%s", p.Author.DisplayName, p.Author.Handle)
 }
 
 func (p Post) Description() string {
-	return p.Content
+	if p.Content != "" {
+		return fmt.Sprintf("%s...", p.Content[:len(p.Content)/2])
+	} else {
+		return ""
+	}
 }
 
 func (p Post) FilterValue() string {
-	return p.Author
+	return p.Author.DisplayName
 }
 
+type view int
+
+const (
+	listView = iota
+	detailView
+)
+
 type model struct {
-	posts list.Model
+	view         view
+	posts        list.Model
+	selectedPost Post
 }
 
 func InitialModel(timeline sdk.Timeline) model {
-	items := make([]list.Item, len(timeline.Feed))
+	feed := slices.DeleteFunc(timeline.Feed, func(el sdk.Feed) bool {
+		return el.Post.Record.Reply != nil
+	})
+
+	items := make([]list.Item, len(feed))
 	width, height, _ := term.GetSize(0)
 
-	for i, post := range timeline.Feed {
+	for i, post := range feed {
 		items[i] = Post{
 			ID:      "id",
 			Content: post.Post.Record.Text,
 			Summary: "",
-			Author:  post.Post.Author.DisplayName,
-			Langs:   post.Post.Record.Langs,
+			Author: Author{
+				DisplayName: post.Post.Author.DisplayName,
+				Handle:      post.Post.Author.Handle,
+			},
+			Langs: post.Post.Record.Langs,
 		}
 	}
 
@@ -66,16 +95,38 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 		switch msg.String() {
+		case "o", "enter":
+			m.selectedPost = m.posts.SelectedItem().(Post)
+			m.view = detailView
+			return m, nil
+		case "esc":
+			if m.view == detailView {
+				m.view = listView
+				return m, nil
+			} else {
+				return m, tea.Quit
+			}
 		case "ctrl-c", "q":
 			return m, tea.Quit
 		}
 	}
 
 	var cmd tea.Cmd
+
 	m.posts, cmd = m.posts.Update(msg)
 	return m, cmd
 }
 
 func (m model) View() string {
-	return m.posts.View()
+	switch m.view {
+	case detailView:
+		view, err := glamour.Render(m.selectedPost.Content, "dark")
+		// TODO: deal with errors in a better way
+		if err != nil {
+			panic(err)
+		}
+		return view
+	default:
+		return m.posts.View()
+	}
 }
